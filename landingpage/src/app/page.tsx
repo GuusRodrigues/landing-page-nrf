fe4c35a773ef4e3f4fc6b48de52b12a657217bea
Gustavo Rodrigues <gustarodrigues862@gmail.com>
2025-01-03 13:07:40 -0300

    preparando para IOS

--- landingpage/src/app/page.tsx
+++ landingpage/src/app/page.tsx
@@ -18,23 +18,36 @@ const LandingPage: React.FC = () => {
       <main className="flex-grow w-full max-w-4xl px-6 py-12">
         <section className="bg-blue-100 shadow-md rounded-lg p-6 mb-8 text-center">
           <h2 className="text-2xl font-semibold text-blue-700">Baixe Agora</h2>
-          <p className="text-md mt-2 text-gray-700">Disponível em formato APK para dispositivos Android.</p>
-          <a
-            href="https://drive.google.com/file/d/1uB6XuiEAqJTdLsGVnZwF-l7NRqnIR6ga/view?usp=sharing"
-            download
-            className="mt-4 inline-block bg-blue-700 text-white py-3 px-6 rounded-full shadow-lg hover:bg-blue-800 transition"
-          >
-            Baixar APK
-          </a>
+          <p className="text-md mt-2 text-gray-700">
+            Disponível para dispositivos Android e iOS.
+          </p>
+          <div className="flex flex-col sm:flex-row justify-center space-y-4 sm:space-y-0 sm:space-x-4">
+            <a
+              href="https://drive.google.com/file/d/1uB6XuiEAqJTdLsGVnZwF-l7NRqnIR6ga/view?usp=sharing"
+              download
+              className="bg-blue-700 text-white py-3 px-6 rounded-full shadow-lg hover:bg-blue-800 transition"
+            >
+              Baixar para Android
+            </a>
+            <a
+              href="/app.apk"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="bg-blue-700 text-white py-3 px-6 rounded-full shadow-lg hover:bg-blue-800 transition"
+            >
+              Baixar para iOS
+            </a>
+          </div>
         </section>
 
         <section className="bg-gray-100 shadow-md rounded-lg p-6">
           <h2 className="text-2xl font-semibold text-blue-700 text-center">Como Usar</h2>
           <ol className="list-decimal list-inside mt-6 text-left text-gray-800 space-y-4">
-            <li>Baixe, dê permissão e instale o aplicativo APK em seu dispositivo Android.</li>
-            <li>Abra o app e navegue pelas palestras e eventos da NRF 2025.</li>
+            <li>Baixe o aplicativo no seu dispositivo (Android ou iOS).</li>
+            <li>Conceda as permissões necessárias e instale o aplicativo.</li>
+            <li>Abra o app e explore as palestras e eventos da NRF 2025.</li>
             <li>Adicione palestras à sua agenda personalizada.</li>
-            <li>Publique suas experiências na linha do tempo e compartilhe com outros participantes.</li>
+            <li>Visualize e interaja com as publicações na linha do tempo.</li>
           </ol>
         </section>
       </main>
